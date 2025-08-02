d72fb3403f1fc4c39b0a7141c446c9bb90648897
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-08-02 12:16:19 +0100

    Initial Commit

---
+++ cryptolive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cryptolive
+//
+//  Created by BSTAR on 02/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cryptolive/cryptoliveApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoliveApp.swift
+//  cryptolive
+//
+//  Created by BSTAR on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct cryptoliveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
