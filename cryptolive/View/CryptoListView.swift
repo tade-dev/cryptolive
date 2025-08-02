a13ece6782b8c96c54c6bb74614ca92e84356096
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-08-02 14:03:37 +0100

    [Features] Added some crypto images, and implemented the combine and timer functionalities

--- cryptolive/View/CryptoListView.swift
+++ cryptolive/View/CryptoListView.swift
@@ -6,18 +6,61 @@
 //
 
 import SwiftUI
+import Combine
 
 struct CryptoListView: View {
+    
+    @StateObject var vm = CryptoListViewModel()
+    let timer = Timer.publish(every: 30, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 LazyVStack {
-                    
+                    ForEach(vm.filteredCryptoList) { crypto in
+                        
+                    }
                 }
             }
             .navigationTitle("Crypto List")
+            .searchable(text: $vm.searchQuery)
+            .onReceive(timer) { value in
+                vm.fetchCryptoData()
+            }
         }
     }
+}
+
+struct CryptoTile: View {
+    
+    let name: String
+    let symbol: String
+    let priceUsd: String
+    let marketCap: String
+    let onTap: OnTap
+    
+    typealias OnTap = ()-> Void
+    
+    var body: some View {
+        HStack {
+            
+            Spacer()
+            buildChangeIndicator(value: priceUsd)
+        }
+    }
+    
+    private func buildChangeIndicator(value: String) -> some View {
+        return Text("\(value.contains("-") ? value : "+\(value)")%")
+            .foregroundStyle(.white)
+            .font(.headline)
+            .padding(.horizontal, 20)
+            .padding(.vertical, 5)
+            .background(
+                RoundedRectangle(cornerRadius: 3)
+                    .fill(value.contains("-") ? Color.red : Color.green)
+            )
+    }
+    
 }
 
 #Preview {
--- cryptolive/ViewModel/CryptoListViewModel.swift
+++ cryptolive/ViewModel/CryptoListViewModel.swift
@@ -10,6 +10,45 @@ import Foundation
 
 class CryptoListViewModel: ObservableObject {
     
+    @Published var searchQuery: String = ""
+    @Published var cryptoList: [Crypto] = []
+    @Published var isLoading: Bool = false
+    @Published var cancellables: Set<AnyCancellable> = []
+    var filteredCryptoList: [Crypto] {
+        if(searchQuery.isEmpty) {
+            return cryptoList
+        } else {
+            return cryptoList.filter { $0.name.lowercased().contains(searchQuery.lowercased()) }
+        }
+    }
+   
+    init() {
+        fetchCryptoData()
+    }
     
+    func fetchCryptoData() {
+        let url = URL(string: "https://rest.coincap.io/v3/assets")
+        
+        guard let url else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global())
+            .receive(on: DispatchQueue.main)
+            .tryMap { (data, response)-> Data in
+                guard
+                    let repsonse = response as? HTTPURLResponse,
+                    response.statusCode >= 200 && response.statusCode < 300 else {
+                        throw URLError(.badServerResponse)
+                    }
+                return data
+            }
+            .decode(type: [Crypto].self, decoder: JSONDecoder())
+            .sink { (completion) in
+                print("COMPLETION:: \(completion)")
+            } receiveValue: { [weak self] cryptoList in
+                self?.cryptoList = cryptoList
+            }
+            .store(in: &cancellables)
+    }
     
 }
